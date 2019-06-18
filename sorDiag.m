d0ce9459b5c73924381e7d4192be23bb5c24ff9f
Mikaella <42984505+mikaellafs@users.noreply.github.com>
2019-06-17 22:49:49 -0300

    Add files via upload

--- sorDiag.m
+++ sorDiag.m
@@ -11,30 +11,30 @@ function [x, iter] = sorDiag(diags,b,tol,nmaxiter,w)
 %
   
   [n,tam] = size(diags);
-  meio = fix(tam/2 + 1);
+  meio = fix(tam/2 +1);
   x0 = zeros(n,1);
   x = x0;
   iter = 0;
   err = tol +1;
-  
-  while err > tol && (iter < nmaxiter)
+
+  while (err > tol) && (iter < nmaxiter)
     for i=1:n
       soma = 0.0;
       for j=1:meio-1
-        aux = meio-j;
-        
-        if (i-aux-3<1) a = 0;
-        else a = x0(i-aux-3);
-        endif
-        if (i+aux+3>n) c = 0;
-        else c = x(i+aux+3);
+        aux1 = i-j;
+        if(aux1<1) a = 0;
+        else a = x(aux1);
         endif
       
-        soma = soma + diags(i,j)*a + diags(i,meio+j)*c;
+        aux2 = i+j;
+        if(aux2>n) c = 0;
+        else c = x0(aux2);
+        endif
+
+        soma = soma + a*diags(i,meio-j) + c*diags(i,meio+j);
       endfor
       x(i) = w*(b(i) - soma)/diags(i,meio) + (1-w)*x0(i);
     endfor
-    err = norm((x-x0)/x);
     iter = iter + 1;
     x0 = x;
   endwhile
---
+++ validacao.m
@@ -0,0 +1,97 @@
+clc
+clear all
+##################### Validação da solução com um problema conhecido #####################
+############# DADOS DO PROBLEMA ################
+hx = 0.5;
+hy = 0.5;
+a = 0;
+b = 10;
+c = 0;
+d = 5;
+%% FUNÇÕES f(x,y) e g(x,y) dadas %%
+function [v] = f(x,y)
+  v =(1/5)*(x*(10-x)+y*(5-y));
+endfunction
+
+function [v] = g(x,y)
+  v=0.625*x*(10-x);
+endfunction
+
+%% FUNÇÃO Vp(x,y) esperada %%
+function [v] = Vp(x,y)
+  v = x*(10-x)*y*(5-y)/10;
+endfunction
+
+#################################################
+nx = (b-a)/hx +1;
+ny = (d-c)/hy +1;
+
+ap = bp = -1/(hx^2);
+cp = dp = -1/(hy^2);
+ep = 2/(ap+cp);
+
+#### Criando vetor de diagonais ####
+diags = [dp,bp,ep,ap,cp];
+for i=1:nx*ny
+  diags(i, :) = [dp,bp,ep,ap,cp];
+endfor
+% zerando b em algumas linhas
+for i=0:(ny-1)
+  diags(i*nx +1, 2) = 0;
+endfor
+% zerando a em algumas linhas
+for i=1:ny
+  diags(i*nx, 4) = 0;
+endfor
+
+#### Criando vetor de coeficientes independentes ####
+n = 1;
+for i=1:nx
+  x = a + (i-1)*hx;
+  for j=1:ny
+    y = c + (j-1)*hy;
+    fp(n) = f(x,y);
+    n = n+1;
+  endfor
+endfor
+
+#### Aplicando condicoes de contorno ####
+% Ajustando para V=0 na fronteira
+zero = zeros(2*(nx+ny), 1);
+[diags, fp] = aplicaCondicoes(a, a, c, d, zero, hy, hx, fp, diags,ny, a, c);
+[diags, fp] = aplicaCondicoes(b, b, c, d, zero, hy, hx, fp, diags,ny, a, c);
+[diags, fp] = aplicaCondicoes(a, b, c, c, zero, hy, hx, fp, diags,ny, a, c);
+[diags, fp] = aplicaCondicoes(a, b, d, d, zero, hy, hx, fp, diags,ny, a, c);
+
+% Ajustando para V= g(x,y) em y= 2.5, 0<x<10
+for i = 1:(nx-1)
+   x = a+(i*hx);
+   gp(i) = g(x,2.5);
+endfor
+
+[diags, fp] = aplicaCondicoes(a+hx, b-hx, 2.5, 2.5, gp, hy, hx, fp, diags,ny, a, c);
+
+#### Resolver o sistema gerado pelo metodo sor ####
+% Calculo de w:
+t = cos(pi/nx) + cos(pi/ny);
+w = (8 - (64-16*t^2)^(1/2))/t^2;
+
+[V,iter] = sorDiag(diags, fp, 10^(-6), 20, w);
+
+#### Calculo do erro a partir do valor esperado ####
+%% Valores esperados %%
+n = 1;
+for i=1:nx
+  x = a + (i-1)*hx;
+  for j=1:ny
+    y = c + (j-1)*hy;
+    Vexato(n, :) = Vp(x,y);
+    n = n+1;
+  endfor
+endfor
+
+%% Erro %%
+for i = 1:nx*ny
+  Vr(i) = abs(Vexato(i)-V(i));
+endfor
+err= max(Vr)
